35098c55ae7bcba1802ab6601c6bb1bddcc0f706
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-05 11:17:08 +0530

    UC3 done to print board

--- src/com/training/tictactoe/TicTacToeGame.java
+++ src/com/training/tictactoe/TicTacToeGame.java
@@ -22,18 +22,35 @@ public class TicTacToeGame {
 	// player choice input
 	private void allowPlayer(Scanner sc) {
 		System.out.println("Input Your Choice X or O");
-		char c = sc.next().charAt(0);
+		char c = sc.next().toUpperCase().charAt(0);
+		if(c!='O'&& c!='X')
+			allowPlayer(sc);
 		playerLetter = c;
 		computerLetter = playerLetter == 'X' ? 'O' : 'X';
+	}
+
+	// Print Board
+	private void printBoard() {
+		for (int i = 1; i < board.length; i++) {
+			if (i % 3 == 1 || i % 3 == 2)
+				System.out.print(board[i] + "  | ");
+			if (i % 3 == 0) {
+				System.out.println(board[i]);
+				if (i != board.length - 1)
+					System.out.println("-------------");
+			}
+		}
 	}
 
 	// main
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game :");
 		Scanner sc = new Scanner(System.in);
-		TicTacToeGame tttg = new TicTacToeGame();
-		tttg.fillBoard();
-		tttg.allowPlayer(sc);
+		TicTacToeGame TTTG = new TicTacToeGame();
+		TTTG.fillBoard();
+		TTTG.allowPlayer(sc);
+		TTTG.printBoard();
 		sc.close();
+
 	}
 }
