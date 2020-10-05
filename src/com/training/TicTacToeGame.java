e48d98ae258bb8ccce1a2286377eeaebc7d9fa59
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-05 09:09:43 +0530

    UC1 class created board created

--- src/com/training/TicTacToeGame.java
+++ src/com/training/TicTacToeGame.java
@@ -1,8 +1,12 @@
 package com.training;
 
 public class TicTacToeGame {
+	char[] board;
+	public TicTacToeGame() {
+		board=new char[10];
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game :");
+		TicTacToeGame tttg=new TicTacToeGame();
 	}
-
 }
