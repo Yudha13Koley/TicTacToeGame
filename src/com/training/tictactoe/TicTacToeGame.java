1a4dce92eef491772f033b3118e97c674f03b30a
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-07 10:35:29 +0530

    Added : Functionality Computer Blocks User Winning Move

--- src/com/training/tictactoe/TicTacToeGame.java
+++ src/com/training/tictactoe/TicTacToeGame.java
@@ -7,6 +7,7 @@ public class TicTacToeGame {
 	char playerLetter;
 	char computerLetter;
 	boolean[] isFree;
+	boolean isPlayerTurn;
 
 	// constructor
 	public TicTacToeGame() {
@@ -32,10 +33,12 @@ public class TicTacToeGame {
 		if (str.equalsIgnoreCase(toss)) {
 			System.out.println("User Goes First !");
 			allowPlayer(sc);
+			isPlayerTurn=true;
 		} else {
 			System.out.println("Computer Goes First !");
 			computerLetter = 'X';
 			playerLetter = 'O';
+			isPlayerTurn=false;
 		}
 	}
 
@@ -47,19 +50,6 @@ public class TicTacToeGame {
 			allowPlayer(sc);
 		playerLetter = c;
 		computerLetter = playerLetter == 'X' ? 'O' : 'X';
-	}
-
-	// Print Board
-	private void printBoard() {
-		for (int i = 1; i < board.length; i++) {
-			if (i % 3 == 1 || i % 3 == 2)
-				System.out.print(board[i] + "  | ");
-			if (i % 3 == 0) {
-				System.out.println(board[i]);
-				if (i != board.length - 1)
-					System.out.println("-------------");
-			}
-		}
 	}
 
 	// Overload printBoard
@@ -86,7 +76,9 @@ public class TicTacToeGame {
 			System.out.println("The position is free to move :");
 			board[position] = playerLetter;
 			isFree[position] = false;
+			System.out.println("Player Move : ");
 			printBoard(board);
+			isPlayerTurn=false;
 		} else {
 			System.out.println("The position is occupied :");
 			makePlayerMove(sc);
@@ -94,25 +86,52 @@ public class TicTacToeGame {
 
 	}
 	// computerMove
-	private void makeComputerMove(Scanner sc) {
-		int position = (int)Math.floor(Math.random()*100)%9+1;
+	private boolean isWinningMove(int position) {
 		if (position < 0 || position > 9) {
 			System.out.println("Enter a valid position :");
-			makeComputerMove(sc);
+			return false;
 		} else if (isFree[position]) {
-			System.out.println("The position is free to move :");
-			board[position] = computerLetter;
-			isFree[position] = false;
-			printBoard(board);
+			char c = board[position];
+			board[position]=playerLetter;
+			boolean b= isWin(playerLetter);
+			board[position]=c;
+			return b;
 		} else {
-			System.out.println("The position is occupied :");
-			makeComputerMove(sc);
+			return false;
 		}
 
 	}
+	//Blocking Player By computer
+	private void makeSmartComputerMove() {
+		boolean b;
+		int winningPos=0;
+		for(int i=1;i<board.length;i++)
+		{
+			b=isWinningMove(i);
+			if(b)
+			{
+				winningPos=i;
+				break;
+			}
+		}
+		if(winningPos==0)
+			winningPos=(int) (Math.floor(Math.random()*100)%9+1);
+		if (winningPos < 0 || winningPos > 9) {
+			System.out.println("Enter a valid position :");
+			makeSmartComputerMove();
+		} else if (isFree[winningPos]) {
+			board[winningPos]=computerLetter;
+			isFree[winningPos]=false;
+			System.out.println("Computer Move :");
+			printBoard(board);
+			isPlayerTurn=true;
+		} else {
+			makeSmartComputerMove();
+		}
+			
+	}
 	//Winning Condition
 	private boolean isWin(char ch) {
-		boolean b=false;
 	if(board[1]==board[2] &&board[2]==board[3]&&board[1]==ch)
 		return true;
 	if(board[4]==board[5] &&board[5]==board[6]&&board[4]==ch)
@@ -139,13 +158,35 @@ public class TicTacToeGame {
 		TicTacToeGame TTTG = new TicTacToeGame();
 		TTTG.fillBoard();
 		TTTG.firstTurn(sc);
+		while(true) {
+		if(TTTG.isPlayerTurn) {
 		TTTG.makePlayerMove(sc);
-		TTTG.makeComputerMove(sc);
-		TTTG.makePlayerMove(sc);
-		TTTG.makeComputerMove(sc);
-		TTTG.makePlayerMove(sc);
-		TTTG.makeComputerMove(sc);
-		System.out.println(TTTG.isWin(TTTG.playerLetter));
+		if(TTTG.isWin(TTTG.playerLetter))
+		{
+			System.out.println("Player Wins !");
+			break;
+		}
+		}
+		else {
+		TTTG.makeSmartComputerMove();;
+		if(TTTG.isWin(TTTG.computerLetter))
+		{
+			System.out.println("Computer Wins !");
+			break;
+		}
+		}
+		int count=0;
+		for(int i=1;i<10;i++)
+		{
+			if(TTTG.isFree[i]==false)
+				count++;
+		}
+		if(count==9)
+		{
+			System.out.println("Draw");
+			break;
+		}
+		}
 		sc.close();
 	}
 }
